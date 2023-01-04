95bd1da9dc1bc4cb99fe34d290943ec7f49be820
Muhammed Yilmaz <yilmaz@email.arizona.edu>
2023-01-04 15:22:16 -0700

    Updated from day 30 to use try block

--- main.py
+++ main.py
@@ -5,5 +5,9 @@ nato_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 # print(nato_dict)
 
 word = input("What word would you like to convert to NATO Alphabet?\n").upper()
-converted = [nato_dict[letter] for letter in word]
-print(converted)
+try:
+    converted = [nato_dict[letter] for letter in word]
+except KeyError:
+    print("Sorry only letters from the alphabet please.")
+else:
+    print(converted)
